e6a7b549552cc9a5e03fed63b4a584eadaf3b888
Lais Lessa <124070111+Lais-Lessa@users.noreply.github.com>
2023-03-30 14:53:02 -0300

    TarefaFinalizada

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,33 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+scroll();
+function scroll() {
+    const button = document.querySelector(".main__button");
+    const section = document.querySelector(".main__about-container");
+
+    button.addEventListener('click', function () {
+        section.scrollIntoView({ behavior: "smooth" })
+    });
+};
+
+modal();
+function modal() {
+    const buttons = document.querySelectorAll(".btn-open-modal");
+    const modal = document.querySelector("#modal");
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', function () {
+            modal.showModal();
+            exitModal();
+        });
+
+    })
+};
+
+function exitModal() {
+    const buttonModal = document.querySelector(".modal__button");
+    const modalDialog = document.querySelector("#modal");
+
+    buttonModal.addEventListener('click', function () {
+        modalDialog.close();
+    });
+};
